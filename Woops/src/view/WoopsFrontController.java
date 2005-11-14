d16b990446e3e27a0ed928a295570dd828dc48b6
ben_woops <ben_woops>
2005-11-14 19:09:53 +0000

    création du servlet WoopsFrontController

---
+++ Woops/src/view/WoopsFrontController.java
@@ -0,0 +1,13 @@
+  import javax.servlet.ServletException 
+  import org.apache.struts.action.ActionServlet; 
+  import com.cc.framework.ui.painter.PainterFactory 
+  import com.cc.framework.ui.painter.def.DefPainterFactory; 
+  import com.cc.framework.ui.painter.html.HtmlPainterFactory; 
+  
+  public class WoopsFrontController extends ActionServlet { 
+  	public void init() throws ServletException { 
+  	super.init(); 
+  	PainterFactory.registerApplicationPainter ( getServletContext (), DefPainterFactory.instance());
+  	PainterFactory.registerApplicationPainter ( getServletContext (), HtmlPainterFactory.instance()); 
+  	} 
+  } 
