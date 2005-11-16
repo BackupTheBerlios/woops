2e03d16151a79e49831be5759ba4cd853a5a927e
audige <audige>
2005-11-16 21:34:10 +0000

    Action permettant d'afficher la liste des activités

--- Woops/src/view/activities/ListActivitiesAction.java
+++ Woops/src/view/activities/ListActivitiesAction.java
@@ -3,6 +3,7 @@ package view.activities;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 import javax.servlet.ServletException;
 
@@ -10,11 +11,15 @@ import org.apache.log4j.Logger;
 
 import org.apache.struts.action.ActionForward;
 
+import business.chocolat.Chocolat;
+import business.format.Formatage;
+
 import com.cc.framework.adapter.struts.ActionContext;
 import com.cc.framework.common.DisplayObject;
 
 import view.PresentationConstantes;
 import view.WoopsCCAction;
+import view.chocolat.ListerChocolatFormItem;
 
 public class ListActivitiesAction extends WoopsCCAction {
 	private static Logger logger = Logger.getLogger(ListActivitiesAction.class);    
@@ -22,20 +27,31 @@ public class ListActivitiesAction extends WoopsCCAction {
 	public void doExecute(ActionContext context) throws IOException, ServletException {
 		logger.debug("ListActivitiesAction");
 		
+		Collection listActivitiesMgr = null;
+		Collection listActivitiesItems = null;
+		ActivityItem item = null;
 		ActionForward retour = null;
     	
 		try {
 	    	ListActivitiesForm listActivitiesForm = (ListActivitiesForm) context.form();
 			
 	    	// Get the Displaydata for our List
-			Collection listActivities = new ArrayList();
-			listActivities.add("activity 1");
-			listActivities.add("activity 2");
-			listActivities.add("activity 3");
-			listActivities.add("activity 4");
+	    	// listActivitiesMgr = ActivitytManager.getInstance().getActivities()
+	    	
+//			Iterator iter = listActivitiesMgr.iterator();
+//			while (iter.hasNext()) {
+//				Activity activity = (Activity)iter.next();
+//				
+				item = new ActivityItem();
+				
+				item.setName("Liste");
+				item.setDescription("Liste");
+				
+				listActivitiesItems.add(item);
+//			}
 			
 			// Convert the List into DisplayObject tab
-			DisplayObject[] data = new DisplayObject[0];
+			DisplayObject[] data = (ActivityItem[]) listActivitiesItems.toArray(new DisplayObject[0]);
 			
 			/* Create the ListControl and populate it.
 			with the Data to be displayed */
