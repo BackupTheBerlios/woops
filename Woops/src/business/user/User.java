f49d85b21bbb35376e47d9349c61507317690476
audige <audige>
2005-11-18 14:22:31 +0000

    Mise à jour des classes métier pour User

--- Woops/src/business/user/User.java
+++ Woops/src/business/user/User.java
@@ -4,19 +4,69 @@ import java.util.Collection;
 
 public class User {
 
-		/**
-	 * @link aggregation 
+	private Integer id;
+
+	private String login;
+
+	private String password;
+
+	private String firstName;
+
+	private String lastName;
+
+	/**
+	 * @link aggregation
 	 * @associates business.activity.Activity
 	 * @directed directed
 	 * @supplierCardinality 0..*
 	 */
 	private Collection linkToActivity = null;
 
-		public Collection getLinkToActivity() {
-			return linkToActivity;
-		}
+	public Collection getLinkToActivity() {
+		return linkToActivity;
+	}
 
-		public void setLinkToActivity(Collection linkToActivity) {
-			this.linkToActivity = linkToActivity;
-		}
+	public void setLinkToActivity(Collection linkToActivity) {
+		this.linkToActivity = linkToActivity;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
