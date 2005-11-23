f6ef04f6e0edc9954c9b9d3777c56d1094215ee4
audige <audige>
2005-11-23 10:08:03 +0000

    Internationalisation

--- Woops/src/view/WoopsFrontController.java
+++ Woops/src/view/WoopsFrontController.java
@@ -2,6 +2,7 @@ package view;
 
 import javax.servlet.ServletException;
 
+import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionServlet;
 
 import com.cc.framework.ui.painter.PainterFactory;
@@ -11,6 +12,8 @@ import com.cc.framework.ui.painter.html.HtmlPainterFactory;
   public class WoopsFrontController extends ActionServlet { 
   	public void init() throws ServletException { 
   	super.init(); 
+  	/* Autorisation de l'internationalisation */ 
+  	getServletContext().setAttribute( com.cc.framework.Globals.LOCALENAME_KEY, "true");
   	PainterFactory.registerApplicationPainter ( getServletContext (), DefPainterFactory.instance());
   	PainterFactory.registerApplicationPainter ( getServletContext (), HtmlPainterFactory.instance()); 
   	} 
