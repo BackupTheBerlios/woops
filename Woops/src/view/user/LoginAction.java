0ed523a4ced1aa5a13f422f056318d0ceb40d8ef
audige <audige>
2005-11-25 13:46:54 +0000

    Adaptation ? Common-controls

--- Woops/src/view/user/LoginAction.java
+++ Woops/src/view/user/LoginAction.java
@@ -1,69 +1,58 @@
 package view.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
 
 import view.PresentationConstantes;
-import view.WoopsAction;
-
+import view.WoopsCCAction;
 import business.hibernate.exception.PersistanceException;
 import business.user.User;
 import business.user.UserManager;
 
-public class LoginAction extends WoopsAction {
+import com.cc.framework.adapter.struts.ActionContext;
 
-	public ActionForward execute(
-			ActionMapping mapping,
-			ActionForm form,
-			HttpServletRequest request,
-			HttpServletResponse response) {
+public class LoginAction extends WoopsCCAction {
 
-			LoginForm loginForm = (LoginForm) form;
+	public void doExecute(ActionContext context) {
+
+			LoginForm loginForm = (LoginForm) context.form();
 
 			ActionForward retour = null;
-			ActionMessages message = new ActionMessages();
 			
-			HttpSession httpSession = request.getSession(false);
+			HttpSession httpSession = context.request().getSession(false);
 			
 			// controle de la validation du formulaire
-			message = loginForm.validate(mapping,request);
+			context.addErrors(loginForm.validate(context.mapping(),context.request()));
 			
-		    if (message.isEmpty()) {
+		    if (!context.hasErrors()) {
 				
 				try {
-					//Controle de la validité du couple login/mot de passe
+					//Controle de la validit? du couple login/mot de passe
 					User user = UserManager.getInstance().isLoginValid(loginForm.getLogin(),loginForm.getPassword());
 					
 					if (user!=null) {
 						//on met en session l'utilisateur
 						httpSession.setAttribute(PresentationConstantes.KEY_USER,user);
 						
-						retour = mapping.findForward(PresentationConstantes.FORWARD_SUCCES);
+						retour = context.mapping().findForward(PresentationConstantes.FORWARD_SUCCES);
 					}
 					else {
-						message.add("message", new ActionMessage("errors.login.invalide"));
-						retour = mapping.findForward(PresentationConstantes.FORWARD_ERREUR);
+						context.addGlobalError("errors.login.invalide");
+						retour = context.mapping().findForward(PresentationConstantes.FORWARD_ERREUR);
 					}
 				} catch (PersistanceException pe) {
                     pe.printStackTrace();
-					message.add("erreur", new ActionMessage("errors.persistance.global"));
+                    context.addGlobalError("errors.persistance.global");
 				}	
 
 	        } else {
-	        	retour = mapping.findForward(PresentationConstantes.FORWARD_ERREUR);
-	        }		
-			
+	        	retour = context.mapping().findForward(PresentationConstantes.FORWARD_ERREUR);
+	        }
 		    
-            saveMessages(request, message);
-	        	
-			return retour; 
+		    context.forward(retour);
 		}
 }
 	
