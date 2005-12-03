7465910860af9fac368d0400359e604ae1ddc016
audige <audige>
2005-12-03 12:23:54 +0000

    Ajout des constantes pour les états

--- Woops/src/business/BusinessConstantes.java
+++ Woops/src/business/BusinessConstantes.java
@@ -9,6 +9,12 @@ public interface BusinessConstantes {
 	public static final String OUI = "O";
 	public static final String NON = "N";
     
+    public static final String LINK_TYPE_FINISH_TO_START = "finishToStart";
+    public static final String LINK_TYPE_FINISH_TO_FINISH = "finishToFinish";
+    public static final String LINK_TYPE_START_TO_START = "startToStart";
+    public static final String LINK_TYPE_START_TO_FINISH = "startToFinish";
     
-    
+    public static final String ACTIVITY_STATE_CREATED = "created";
+    public static final String ACTIVITY_STATE_IN_PROGRESS = "inProgress";
+    public static final String ACTIVITY_STATE_FINISHED = "finished";
 }
