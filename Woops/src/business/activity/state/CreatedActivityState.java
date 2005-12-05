fd73b5feebf15916a14e14a13cbb5fd9c13788ae
audige <audige>
2005-12-05 08:19:22 +0000

    Implémntation du CU_part_5_1

--- Woops/src/business/activity/state/CreatedActivityState.java
+++ Woops/src/business/activity/state/CreatedActivityState.java
@@ -1,10 +1,6 @@
 package business.activity.state;
 
-import java.util.Iterator;
-
-import business.BusinessConstantes;
 import business.activity.Activity;
-import business.activity.ActivitySequence;
 
 /**
  * Implements a behavior associated with a state of the Context.
@@ -18,33 +14,33 @@ public class CreatedActivityState extends IActivityState {
 
 	public boolean process(Activity activity) {
 		boolean result = false;
-		if (checkBeforeChange(activity)) {
-			activity.setState(new InProgressActivityState());
-			result = true;
-		}
+//		if (checkBeforeChange(activity)) {
+//			activity.setState(new InProgressActivityState());
+//			result = true;
+//		}
 		return result;
 	}
 
 	public boolean checkBeforeChange(Activity activity) {
-boolean result = true;
+		boolean result = true;
 		
-		Iterator iter = activity.getListActivitiesSequences().iterator();
-		
-		ActivitySequence actSeq = null;
-		while ((iter.hasNext())&&(result==true)) {
-			actSeq = (ActivitySequence)iter.next();
-			
-			if (actSeq.getLinkType().equals(BusinessConstantes.LINK_TYPE_FINISH_TO_START)) {
-				if (!(actSeq.getPredecessor().getState() instanceof FinishedActivityState))
-					result = false;
-			}
-			
-			if (actSeq.getLinkType().equals(BusinessConstantes.LINK_TYPE_START_TO_START)) {
-				if (!(actSeq.getPredecessor().getState() instanceof InProgressActivityState))
-					result = false;
-			}
-		}
-		
+//		Iterator iter = activity.getListActivitiesSequences().iterator();
+//		
+//		ActivitySequence actSeq = null;
+//		while ((iter.hasNext())&&(result==true)) {
+//			actSeq = (ActivitySequence)iter.next();
+//			
+//			if (actSeq.getLinkType().equals(BusinessConstantes.LINK_TYPE_FINISH_TO_START)) {
+//				if (!(actSeq.getPredecessor().getState() instanceof FinishedActivityState))
+//					result = false;
+//			}
+//			
+//			if (actSeq.getLinkType().equals(BusinessConstantes.LINK_TYPE_START_TO_START)) {
+//				if (!(actSeq.getPredecessor().getState() instanceof InProgressActivityState))
+//					result = false;
+//			}
+//		}
+//		
 		return result;
 	}
 	
