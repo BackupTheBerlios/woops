dcb6a13dda86fcc6973ad2790daa64d333f792ec
sregg <sregg>
2006-01-26 21:07:24 +0000

    Correction navigation manageActivity
    Ajout d'une fiche de détail d'une activité lorsque l'on clique sur le nom de l'activité dans la liste

--- Woops/src/view/PresentationConstantes.java
+++ Woops/src/view/PresentationConstantes.java
@@ -14,6 +14,7 @@ public interface PresentationConstantes {
 	public static final String FORWARD_RETOUR="retour";
     public static final String FORWARD_DECONNECT="deconnect"; 
     public static final String FORWARD_EDIT="edit";
+    public static final String FORWARD_DRILLDOWN="drillDown";
     public static final String FORWARD_BACK="back"; 
 	public static final String FORWARD_INDEX="index"; 
 	public static final String FORWARD_NOSESSION="nosession";
@@ -55,8 +56,7 @@ public interface PresentationConstantes {
     //Constantes permettant de connaitre le mode d'un formulaire ( create ou update )
     public static final String INSERT_MODE = "insert_mode";
     public static final String UPDATE_MODE = "update_mode";
-    
-    // Constantes forward pour admin
-    public static final String FORWARD_ADMIN="admin";
 	
+	// Constantes forward pour admin
+    public static final String FORWARD_ADMIN="admin";
 }
