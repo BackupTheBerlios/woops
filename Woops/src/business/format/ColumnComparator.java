7d4d4cb4f32621581244d2be782fe2ed0ac68620
audige <audige>
2006-01-27 00:17:04 +0000

    Classe permettant de trier une liste

--- Woops/src/business/format/ColumnComparator.java
+++ Woops/src/business/format/ColumnComparator.java
@@ -1,22 +1,19 @@
 package business.format;
 
 import java.util.Comparator;
-
-import org.apache.commons.beanutils.PropertyUtils;
-
 import com.cc.framework.common.SortOrder;
 
-public class ColumnComparator implements Comparator {
+public abstract class ColumnComparator implements Comparator {
 
 	/**
 	 * Elément à comparer
 	 */
-	private	String	column = "";
+	protected	String	column = "";
 	
 	/**
 	 * Ordre de tri
 	 */
-	private	SortOrder 	direction = SortOrder.NONE;
+	protected	SortOrder 	direction = SortOrder.NONE;
 
 	/**
 	 * Constructeur
@@ -33,23 +30,5 @@ public class ColumnComparator implements Comparator {
 	/**
 	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
 	 */
-	public int compare(Object o1, Object o2) {
-		
-		int result = 0;
-			
-		try {	
-			String str1 = (String) PropertyUtils.getProperty(o1, column);
-			String str2 = (String) PropertyUtils.getProperty(o2, column);
-		
-			if (direction.equals(SortOrder.ASCENDING)) {
-				result = str1.compareTo(str2);
-			} else if (direction.equals(SortOrder.DESCENDING)) {
-				result = str2.compareTo(str1);
-			}
-		} catch (Throwable t) {}
-
-		return result;
-	}
-
-
+	public abstract int compare(Object o1, Object o2);
 }
