823a9824471fd90894a6bd3c95401ae228691e8a
audige <audige>
2006-02-03 15:27:22 +0000

    On place un User en session pour chaque jeu de tests

--- Woops/src/test/action/TestActionWoops.java
+++ Woops/src/test/action/TestActionWoops.java
@@ -6,6 +6,9 @@ package test.action;
 import java.io.File;
 
 import servletunit.struts.MockStrutsTestCase;
+import view.PresentationConstantes;
+import business.user.User;
+import business.user.UserManager;
 
 /**
  * @author Simon REGGIANI
@@ -25,10 +28,11 @@ public class TestActionWoops extends MockStrutsTestCase {
 	 */
 	public void setUp() throws Exception {
 		super.setUp();
-		/* On pr?cise le contexte /WebContent
+		/* On precise le contexte /WebContent
 		pour trouver le fichier /WEB-INF/web.xml */
 		setContextDirectory(new File("WebContent"));
-		
-		
+		// Récupération du User admin et on le place en session 
+		User user = UserManager.getInstance().isLoginValid("woops", "woops");
+		getSession().setAttribute(PresentationConstantes.KEY_USER, user);
 	}
 }
