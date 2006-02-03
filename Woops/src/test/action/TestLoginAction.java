4d9c0fcbb370d62c0c398d6136085e4aca20f06e
audige <audige>
2006-02-03 15:17:47 +0000

    Test de l'action LoginAction

--- Woops/src/test/action/TestLoginAction.java
+++ Woops/src/test/action/TestLoginAction.java
@@ -1,12 +1,9 @@
 package test.action;
 
-import java.io.File;
-
-import business.user.User;
-
 import servletunit.struts.MockStrutsTestCase;
 import view.PresentationConstantes;
 import view.logon.LoginForm;
+import business.user.User;
 
 public class TestLoginAction extends MockStrutsTestCase {
 	private LoginForm loginForm;
@@ -17,10 +14,6 @@ public class TestLoginAction extends MockStrutsTestCase {
 	
 	public void setUp() throws Exception {
 		super.setUp();
-		/* On précise le contexte /WebContent
-		pour trouver le fichier /WEB-INF/web.xml */
-		setContextDirectory(new File("WebContent"));
-
 	}
 	
 	public void testSuccessfullLogin() {
@@ -41,9 +34,55 @@ public class TestLoginAction extends MockStrutsTestCase {
 		assertNotNull(user);
         assertEquals("bernard", user.getLogin());
         assertEquals("bernard", user.getPassword());
-        //On vérifie qu'il n'y a pas d'erreurs et on s'assure que le forward correspond à ce qui est attendu
+        // On vérifie qu'il n'y a pas d'erreurs et on s'assure que le forward correspond à ce qui est attendu
 		verifyForward(user.getRole().getCode());
         verifyNoActionErrors();
 
+	}
+	
+	public void testFailedLogin() {
+		String [] errors = new String[1];
+		errors[0] = "errors.login.invalide";
+		setRequestPathInfo("/loginUser");
+		
+		//On constitue le form et on initialise les paramètres
+		loginForm = new LoginForm();
+		loginForm.setLogin("befgrd");
+		loginForm.setPassword("bernard");
+		//On fournit l'actionForm à l'action
+		setActionForm(loginForm);
+		
+		//On execute l'action
+		actionPerform();
+
+		// On vérifie qu'il n'y a pas d'User stocké en session
+		User user = (User) getSession().getAttribute(PresentationConstantes.KEY_USER);
+		assertNull(user);
+        // On vérifie qu'il n'y a pas d'erreurs et on s'assure que le forward correspond à ce qui est attendu
+		verifyForward(PresentationConstantes.FORWARD_ERROR);
+        verifyActionErrors(errors);
+	}
+	
+	public void testEmptyForm() {
+		String [] errors = new String[1];
+		errors[0] = "errors.champ.obligatoire";
+		setRequestPathInfo("/loginUser");
+		
+		//On constitue le form et on initialise les paramètres
+		loginForm = new LoginForm();
+		loginForm.setLogin("");
+		loginForm.setPassword("bernard");
+		//On fournit l'actionForm à l'action
+		setActionForm(loginForm);
+		
+		//On execute l'action
+		actionPerform();
+
+		// On vérifie qu'il n'y a pas d'User stocké en session
+		User user = (User) getSession().getAttribute(PresentationConstantes.KEY_USER);
+		assertNull(user);
+        // On vérifie qu'il n'y a pas d'erreurs et on s'assure que le forward correspond à ce qui est attendu
+		verifyForward(PresentationConstantes.FORWARD_ERROR);
+        verifyActionErrors(errors);
 	}
 }
