e1760bae75e1d900a54dbd25137f5f298c56982f
ben_woops <ben_woops>
2006-02-09 10:11:05 +0000

    MAJ theme Woops pour CC

--- Woops/src/view/common/WoopsFrontController.java
+++ Woops/src/view/common/WoopsFrontController.java
@@ -6,6 +6,7 @@ import org.apache.struts.action.ActionServlet;
 
 import com.cc.framework.ui.painter.PainterFactory;
 import com.cc.framework.ui.painter.def.DefPainterFactory;
+import com.cc.framework.ui.painter.def2.Def2PainterFactory;
 import com.cc.framework.ui.painter.html.HtmlPainterFactory;
   
   public class WoopsFrontController extends ActionServlet { 
@@ -15,7 +16,7 @@ import com.cc.framework.ui.painter.html.HtmlPainterFactory;
   	super.init(); 
   	/* Autorisation de l'internationalisation */ 
   	getServletContext().setAttribute( com.cc.framework.Globals.LOCALENAME_KEY, "true");
-  	PainterFactory.registerApplicationPainter ( getServletContext (), DefPainterFactory.instance());
+  	PainterFactory.registerApplicationPainter ( getServletContext (), Def2PainterFactory.instance());
   	PainterFactory.registerApplicationPainter ( getServletContext (), HtmlPainterFactory.instance()); 
   	} 
   } 
