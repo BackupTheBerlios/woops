1019e23e7d5097b39bda667bec3aa08ca5fa494a
sakamakak <sakamakak>
2006-02-10 13:00:28 +0000

    *** empty log message ***

--- Woops/src/view/admin/AdminForm.java
+++ Woops/src/view/admin/AdminForm.java
@@ -1,12 +1,13 @@
 package view.admin;
 
-import com.cc.framework.adapter.struts.FWActionForm;
 import com.cc.framework.ui.control.SimpleListControl;
 import com.cc.framework.ui.model.ListDataModel;
+import com.cc.framework.adapter.struts.FWActionForm;
 
 public class AdminForm extends FWActionForm {
 	
 	private SimpleListControl listUsers = new SimpleListControl();
+	private SimpleListControl listBreakDownElements = new SimpleListControl();
 	
 	public SimpleListControl getListUsers() {
 		return listUsers;
@@ -14,5 +15,13 @@ public class AdminForm extends FWActionForm {
 	
 	public void setDataModelUser(ListDataModel dataModel) {
 		listUsers.setDataModel(dataModel);
+	}
+	
+	public SimpleListControl getListBreakDownElements() {
+		return listBreakDownElements;
+	}
+	
+	public void setDataModelListBreakDownElements(ListDataModel dataModel) {
+		listBreakDownElements.setDataModel(dataModel);
 	}
 }
