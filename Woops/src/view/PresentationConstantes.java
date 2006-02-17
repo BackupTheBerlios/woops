efa4e37302a5ff660ddae322b85be61cd9ada78d
frank28_nfls <frank28_nfls>
2006-02-17 12:23:22 +0000

    *** empty log message ***

--- Woops/src/view/PresentationConstantes.java
+++ Woops/src/view/PresentationConstantes.java
@@ -16,6 +16,8 @@ public interface PresentationConstantes {
     public static final String FORWARD_DECONNECT="deconnect";
     public static final String FORWARD_LOGOUT="logout"; 
     public static final String FORWARD_EDIT="edit";
+    public static final String FORWARD_DELETE="delete";
+    public static final String FORWARD_DELETE_USER="deleteUser";
     public static final String FORWARD_DRILLDOWN="drillDown";
     public static final String FORWARD_BACK="back"; 
 	public static final String FORWARD_INDEX="index"; 
@@ -63,6 +65,7 @@ public interface PresentationConstantes {
     //Constantes permettant de connaitre le mode d'un formulaire ( create ou update )
     public static final String INSERT_MODE = "insert_mode";
     public static final String UPDATE_MODE = "update_mode";
+    public static final String DELETE_MODE = "delete_mode";
     
     // Constantes forward pour admin
     public static final String FORWARD_ADMIN="admin";   
