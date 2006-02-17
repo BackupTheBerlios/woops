0a8aa6b37ffe7e0a5267288b20ea493321f4563e
frank28_nfls <frank28_nfls>
2006-02-17 14:52:41 +0000

    *** empty log message ***

--- Woops/src/view/PresentationConstantes.java
+++ Woops/src/view/PresentationConstantes.java
@@ -18,6 +18,7 @@ public interface PresentationConstantes {
     public static final String FORWARD_EDIT="edit";
     public static final String FORWARD_DELETE="delete";
     public static final String FORWARD_DELETE_USER="deleteUser";
+    public static final String FORWARD_DELETE_BREAKDOWN="deleteBreakdown";
     public static final String FORWARD_DRILLDOWN="drillDown";
     public static final String FORWARD_BACK="back"; 
 	public static final String FORWARD_INDEX="index"; 
@@ -46,7 +47,6 @@ public interface PresentationConstantes {
 	//constantes de cl?s en session
 	public static final String KEY_FORM="FORM";
 	public static final String KEY_USER="USER";
-	public static final String KEY_BDE="BDE_ID";
 	public static final String KEY_OLD_DEPENDANCES_KEYS="OLD_DEPENDANCES_KEYS";
 	public static final String KEY_POSSIBLE_DEPENDANCES_OPTIONS="POSSIBLE_DEPENDANCES_OPTIONS";
 	public static final String KEY_DEPENDANCES_LIST = "KEY_DEPENDANCES_LIST";
@@ -88,5 +88,4 @@ public interface PresentationConstantes {
 	
 	// admin
 	public static final String KEY_USER_ID="USER_ID" ;
-
 }
