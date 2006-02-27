768e7f4721e4ea9df6201be84085bbd1813f0de7
audige <audige>
2006-02-27 12:26:44 +0000

    Suppression des accents

--- Woops/sql/insert_base.sql
+++ Woops/sql/insert_base.sql
@@ -11,17 +11,17 @@ INSERT INTO `UserBDE` (`bde`, `user`) SELECT b.id, u.id FROM `BreakdownElement` 
 INSERT INTO `UserBDE` (`bde`, `user`) SELECT b.id, u.id FROM `BreakdownElement` as b, `User` as u WHERE `prefix`='W' AND `lastName`='Aubry' AND `firstName`='Claude';
 INSERT INTO `UserBDE` (`bde`, `user`) SELECT b.id, u.id FROM `BreakdownElement` as b, `User` as u WHERE `prefix`='T' AND `lastName`='Aubry' AND `firstName`='Claude';
 
-INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Analyser le problème', '', 1, 'created', 1 );
+INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Analyser le probleme', '', 1, 'created', 1 );
 INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Identifier cas d''utilisation', '', 1, 'created', 1 );
-INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Spécifier cas d''utilisation', '', 1, 'created', 1 );
+INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Specifier cas d''utilisation', '', 1, 'created', 1 );
 INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Organiser exigences', '', 2, 'created', 1 );
 INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Analyser architecture', '', 2, 'created', 1 );
-INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Réaliser le CU_part_2', 'Afficher les activités à réaliser', 1, 'created', 1 );
-INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Programmer la solution', 'Implémenter les cas d''utilisation', 1, 'created', 1 );
-INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Programmer les tests développeur', '', 1, 'created', 1 );
-INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Dérouler l''itération', '', 2, 'created', 1 );
+INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Realiser le CU_part_2', 'Afficher les activites a realiser', 1, 'created', 1 );
+INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Programmer la solution', 'Implementer les cas d''utilisation', 1, 'created', 1 );
+INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Programmer les tests developpeur', '', 1, 'created', 1 );
+INSERT INTO `Activity` (`name`, `details`, `user`, `state`, `bde` ) VALUES  ( 'Derouler l''iteration', '', 2, 'created', 1 );
 
 INSERT INTO `ActivitySequence` (`predecessor`,`successor`,`linkType`) VALUES ( 1,2,1 );
 
 
-INSERT INTO `Activity` (`name`, `state`, `bde` ) VALUES  ( 'LIBRE COMME L AIR !', 'created', 1 );
+INSERT INTO `Activity` (`name`, `state`, `bde` ) VALUES  ( 'Programmer les tests unitaires !', 'created', 1 );
