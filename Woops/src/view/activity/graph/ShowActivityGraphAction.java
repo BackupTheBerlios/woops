a99582b94eef2ad0932a151d5dcd961bbc880d66
sregg <sregg>
2006-02-27 13:21:00 +0000

    Gestion des activit?s libres dans le graphe.

--- Woops/src/view/activity/graph/ShowActivityGraphAction.java
+++ Woops/src/view/activity/graph/ShowActivityGraphAction.java
@@ -23,7 +23,7 @@ import com.cc.framework.adapter.struts.ActionContext;
 
 /**
  * @author Simon REGGIANI
- * ShowActivityGraphAction : permet de voir le graphe des activités du projet
+ * ShowActivityGraphAction : permet de voir le graphe des activit?s du projet
  */
 public class ShowActivityGraphAction extends WoopsCCAction {
 	/**
@@ -48,19 +48,28 @@ public class ShowActivityGraphAction extends WoopsCCAction {
     	User sessionUser = (User) context.session().getAttribute(PresentationConstantes.KEY_USER);
     	
 		try {
+			// D?claration des managers utilis?s
+			ActivityManager activityManager = ActivityManager.getInstance();
+			UserManager userManager = UserManager.getInstance();
+			ActivitySequenceManager activitySequenceManager = ActivitySequenceManager.getInstance();
+			
+			// On utilise la classe GraphViz comme API
 			GraphViz gv = new GraphViz();
 			
+			// On ?crit le debut de graph
 			gv.addln(gv.start_graph());
             
-            UserManager userManager = UserManager.getInstance();
-
+			
+			// On r?cup?re la liste des utilisateur du projet
             Collection listUsers = userManager.getUsersByBDE(sessionUser.getDefaultBDEId());
             
+            // Pour chaque utilisateur, on va afficher son cadre et ses activit?s
             Iterator iterUsers = listUsers.iterator();
             int i = 0;
             while(iterUsers.hasNext()) {
             	User user = (User)iterUsers.next();
             	
+            	// Affichage du cadre ( "cluster" )
             	gv.addln("\tsubgraph cluster"+i+" {");
             	if ( user.getId().equals(sessionUser.getId()) ) {
             		gv.addln("\t\tstyle=filled;");
@@ -69,8 +78,10 @@ public class ShowActivityGraphAction extends WoopsCCAction {
             		
             	gv.addln("\t\tlabel = \""+user.getFirstName()+" "+user.getLastName()+"\";");
             	
-				ActivityManager activityManager = ActivityManager.getInstance();
+            	// R?cup?ration des activit?s de l'utilisateur
 				ArrayList listActivities = (ArrayList) activityManager.getAllActivitiesByUserByBDE((Integer)user.getId(),sessionUser.getDefaultBDEId());
+				
+				// Pour chaque activit?, on va l'afficher
 				Iterator iterActivities = listActivities.iterator();
 				while(iterActivities.hasNext()){
 					Activity act = (Activity)iterActivities.next();
@@ -86,13 +97,50 @@ public class ShowActivityGraphAction extends WoopsCCAction {
 					gv.addln("\t\t"+act.getId().toString()+"["+label+" "+color+" "+style+"];");
 				}
 				
+				// On ferme le cadre
 				gv.addln("\t}");
 				
 				i++;
             }
 			
-			ActivitySequenceManager activitySequenceManager = ActivitySequenceManager.getInstance();
+            
+            // On va afficher ?galement les activit?s libres du projet ( dans un cadre a part )
+            
+            
+            //Affichage du cadre ( "cluster" )
+        	gv.addln("\tsubgraph cluster"+(i)+" {");
+        	String nameFreeLocalized = "Activit?s libres";
+        	// TODO : traduire le string nameFreeLocalized
+        	gv.addln("\t\tlabel = \""+nameFreeLocalized+"\";");
+        	
+      
+            // R?cup?ration de la liste des activit?s libres du projet
+            ArrayList listFreeActivities = (ArrayList) activityManager.getFreeActivities(sessionUser.getDefaultBDEId());
+            
+            //Pour chaque activit? libre, on va l'afficher
+			Iterator iterFreeActivities = listFreeActivities.iterator();
+			while(iterFreeActivities.hasNext()){
+				Activity act = (Activity)iterFreeActivities.next();
+				String label = "label=\""+act.getName()+"\",";
+				String color="";
+				if (act.getState().equals(BusinessConstantes.ACTIVITY_STATE_CREATED))
+					color="color="+PresentationConstantes.COLOR_ACTIVITY_CREATED+",";
+				if (act.getState().equals(BusinessConstantes.ACTIVITY_STATE_IN_PROGRESS))
+					color="color="+PresentationConstantes.COLOR_ACTIVITY_IN_PROGRESS+",";
+				if (act.getState().equals(BusinessConstantes.ACTIVITY_STATE_FINISHED))
+					color="color="+PresentationConstantes.COLOR_ACTIVITY_FINISHED+",";
+				String style="style=filled";
+				gv.addln("\t\t"+act.getId().toString()+"["+label+" "+color+" "+style+"];");
+			}
+			
+            // On ferme le cadre
+			gv.addln("\t}");
+            
+            
+            // On r?cup?re les d?pendances entre les activit?s
 			ArrayList listAllActivitySequences = (ArrayList) activitySequenceManager.getActivitySequencesByBDE(sessionUser.getDefaultBDEId());
+			
+			// Pour chaque d?pendances, on les affiche
 			Iterator iterActivitySequences = listAllActivitySequences.iterator();
 			while(iterActivitySequences.hasNext()){
 				ActivitySequence actSeq = (ActivitySequence)iterActivitySequences.next();
@@ -106,24 +154,28 @@ public class ShowActivityGraphAction extends WoopsCCAction {
 				gv.addln("\t"+predecessorId+" -> "+successorId+"[label=\""+linkType+"\"];");
 			}
 			
+			
+			// On affiche la fin de graph
 			gv.addln(gv.end_graph());
 			
-			//logger.debug(gv.getDotSource());
-			
-			
+			// On r?cup?re le dossier "graph" dans lequel se trouveront les images
 			String graphRealPath = getServlet().getServletContext().getRealPath("/") + "graph" + File.separator;
 			
-			GraphViz.setDOT(graphRealPath+"dot.exe");
+			// Parametrage de la classe GraphViz
+			GraphViz.setDOT("/usr/local/bin/dot");
 			GraphViz.setTEMP_DIR(graphRealPath);
 			
+			// Cr?ation du fichier image : graph + id du user + id du projet
 			String imageFile = "graph"+sessionUser.getId().toString()+sessionUser.getDefaultBDEId().toString()+".jpg";
-			
 			File out = new File(graphRealPath+imageFile);
+			
+			// G?n?ration du fichier image
 			gv.writeGraphToFile(gv.getGraph(gv.getDotSource()), out);
 			
+			// On met ? jour l'attribut du form qui contient le path de l'image g?n?r?e
 			String graphPath = context.request().getContextPath()+"/graph/";
-			
 			form.setImageFilePath(graphPath+imageFile);
+			
 			context.forwardByName(PresentationConstantes.FORWARD_SUCCESS);
 		} catch (PersistanceException pe) {
 			context.addGlobalError("errors.persistance.global");
