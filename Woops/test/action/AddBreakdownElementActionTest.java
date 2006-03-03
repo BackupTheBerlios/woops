f4d4d9acaa6b8ae85140368c3caeea5b4858eba6
castel <castel>
2006-03-03 14:57:09 +0000

    *** empty log message ***

---
+++ Woops/test/action/AddBreakdownElementActionTest.java
@@ -0,0 +1,50 @@
+package action;
+
+import java.io.File;
+
+import business.user.User;
+
+import view.PresentationConstantes;
+import view.activity.performing.ListActivitiesForm;
+import view.admin.breakdownelement.AddBreakdownElementForm;
+import view.admin.user.ListUsersModel;
+import view.logon.LoginForm;
+import action.WoopsActionTest;
+
+public class AddBreakdownElementActionTest extends WoopsActionTest {
+	private AddBreakdownElementForm addForm;
+	
+	public AddBreakdownElementActionTest() {
+		super();
+	}
+	
+	public void setUp() throws Exception {
+		super.setUp();
+		super.connect("woops", "woops");
+	}
+	
+	/*
+	 * Test method for 'view.admin.breakdownelement.AddBreakdownElementAction.doExecute(ActionContext)'
+	 */
+	public void testCreateBreakdownElement() {
+		setRequestPathInfo("/addBreakdownElement");
+		getSession().setAttribute(PresentationConstantes.PARAM_MODE,PresentationConstantes.INSERT_MODE);
+		//On execute l'action
+		actionPerform();
+		
+		// On v?rifie les informations dans le form
+		addForm = (AddBreakdownElementForm) getActionForm();
+		assertNotNull(addForm);		
+		ListUsersModel lst = (ListUsersModel)addForm.getUserParticipationOptions();
+		assertNotNull(lst);
+        verifyNoActionErrors();
+	}
+
+	/*
+	 * Test method for 'view.admin.breakdownelement.AddBreakdownElementAction.add_onClick(FormActionContext)'
+	 */
+	public void testAdd_onClick() {
+		
+	}
+
+}
